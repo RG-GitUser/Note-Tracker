4f820a7395fa30b38a4de3a5cddd635ca7248920
Riley <rileyg0035@gmail.com>
2023-10-11 19:39:58 -0300

    added variables and middleware data for json

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,3 +1,5 @@
+//declaring our variables 
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -5,4 +7,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-    
+//middleware for JSON data 
+app.use(express.json());
+
+//static files 
+app.use(express.static('public'));
+
+
+//start the server ('PORT')
+app.listen(PORT, () => {
+    console.log('Serve is running on port ${PORT}');
+});
+
