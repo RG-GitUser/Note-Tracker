e14f62a28675559ed452a739f55a4c94764b3869
Riley <rileyg0035@gmail.com>
2023-10-16 19:49:28 -0300

    Attempting to fix code to open notes page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Variables declared
 const express = require('express');
 const path = require('path');
-const app = express();
 const api_route = require('./routes/api-route');
 const html_route = require('./routes/html-route');
 const PORT = process.env.PORT || 5500; 
+const app = express();
 
 // Middleware for static files from the 'public' directory 
 app.use(express.static('public'));
@@ -15,11 +15,15 @@ app.use('/', html_route);
 
 // route to "notes.html" file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '/assets/notes.html'));
 });
+
 
 // starting the server - listening for requests on the specifc port 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
 
